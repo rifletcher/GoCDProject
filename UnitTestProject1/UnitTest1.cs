3615144f46615042a8c55697c19901dfb2ef4380
Robert Fletcher <robert.fletcher@justshoutgfs.com>
2018-01-03 16:25:31 +0200

    fixed

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace UnitTestProject1
         public void TestMethod2()
         {
             var result = _valuesController.Get();
-            Assert.IsNull(result, "I should not be Null");
+            Assert.IsNotNull(result, "I should not be Null");
         }
     }
 }
